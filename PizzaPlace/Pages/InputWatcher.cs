bbf5d8d04aa21e6c046205c6912bd5231d29e565
agaliev <hijhog47@gmail.com>
2020-11-07 22:36:04 +0600

    fixed Index component

---
+++ PizzaPlace/Pages/InputWatcher.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaPlace.Pages
+{
+   public class InputWatcher : ComponentBase
+   {
+      private EditContext editContext;
+      [CascadingParameter]
+      public EditContext EditContext 
+      {
+         get => editContext;
+         set
+         {
+            this.editContext = value;
+            EditContext.OnFieldChanged += async (sender, e)
+               =>
+            {
+               await FieldChanged.InvokeAsync(e.FieldIdentifier.FieldName);
+            };
+         } 
+      }
+
+      [Parameter]
+      public EventCallback<string> FieldChanged { get; set; }
+
+      public bool Validate()
+         => EditContext?.Validate() ?? false;
+   }
+}
